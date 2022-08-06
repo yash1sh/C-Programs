a6ddbaaa7078eb723b8566f6d2d11ba1c36a4af8
Yash_Shhhhh <66167011+yash1sh@users.noreply.github.com>
2022-08-06 19:13:03 +0530

    Add files via upload
    new c file

---
+++ objectsinimage.c
@@ -0,0 +1,24 @@
+/*write a program to extract and store the coordinates of the difffrent objects/segements
+in the image . consider 4 connectivity  an object is a grp of cells which are connected to each other.
+example 
+011000
+111001
+110011
+000100
+110111
+there are 4 objects in the matrix
+they are
+011000
+111002
+110022
+000300
+440333
+*/
+main()
+{
+    int rows,cols;
+    printf("enter the rows and cols");
+    scanf("%d %d",&rows,&cols);
+
+}
+

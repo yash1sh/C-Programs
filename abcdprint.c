38959ebb86877a1c97a274f910afe2a1e2b96d45
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-08-11 23:51:05 +0530

    Update abcdprint.c

--- abcdprint.c
+++ abcdprint.c
@@ -6,6 +6,12 @@ int main()
     for(j=0;j<i;j++)
     printf("%c",(var+j));
     printf("\n");
+    
+    var='a'
+    for(i=4;i>=1;i--){
+    for(j=0;j<i;j++)
+    printf("%c",(var+j));
+    printf("\n");
     }
     return 0;
 }
